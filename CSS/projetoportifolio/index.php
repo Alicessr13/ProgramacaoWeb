9c38d5231e697b454997372cea10bc844af411fb
Alice Stephanie Soares Ribeiro <alicessr12@gmail.com>
2023-10-26 22:57:30 -0300

    .

---
+++ CSS/projetoportifolio/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+
+    <link rel="stylesheet" href="estilo.css">
+</head>
+
+<body>
+
+    <?php
+include('cabecalho.html');
+?>
+    <div class="descricao">
+        <span style="font-size: 22px;"><strong>Sobre Alice</strong></span>
+        <p>
+            I don't mind
+            Letting you down easy, but just give it time
+            If it don't hurt now, well, just wait, just wait a while
+            You're not the big fish in the pond no more
+            You are what they're feeding on
+        </p>
+        <p>
+            So what are you gonna do
+            When the world don't orbit around you?
+            So what are you gonna do
+            When the world don't orbit around you?
+        </p>
+        <p>
+            Ain't it fun
+            Living in the real world?
+            Ain't it good
+            Being all alone?
+            Where you're from
+            You might be the one who's running things
+            Well, you could ring anybody's bell and get what you want
+            You see, it's easy to ignore trouble
+            When you're living in a bubble
+        </p>
+
+        <p>
+            Don't go crying to your mama
+            'Cause you're on your own in the real world
+            Don't go crying to your mama
+            'Cause you're on your own in the real world
+
+            Ain't it fun? Ain't it fun?
+            Baby, now you're one of us
+            Ain't it fun? Ain't it fun? Ain't it fun?
+
+            Ain't it fun
+            Living in the real world?
+            Ain't it good (ain't it good)
+            Being all alone?
+
+            Ain't it fun
+            Living in the real world?
+            ('Cause the world don't orbit around you)
+            Ain't it good (ain't it good)
+            Being all alone?
+
+            Don't go crying to your mama (not to your motha)
+            'Cause you're on your own in the real world (don't go crying)
+            Don't go crying to your mama (to your mama)
+            'Cause you're on your own (hey) in the real world (oh)
+
+            Don't go crying to your mama (yeah, yeah)
+            'Cause you're on your own (this is the real world) in the real world
+            Don't go crying to your mama (don't go crying)
+            'Cause you're on your own in the real world (this is the real world, this is the real world)
+        </p> 
+    </div>
+
+    <?php
+include('cabecalho.html');
+?>
+</body>
+
+</html>

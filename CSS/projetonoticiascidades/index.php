aefe211cd67929e456cad19db46744e070568367
Alice Stephanie Soares Ribeiro <alicessr12@gmail.com>
2024-02-06 17:53:14 -0300

    .

---
+++ CSS/projetonoticiascidades/index.php
@@ -0,0 +1,44 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="estilo.css">
+</head>
+
+<body class="home">
+
+    <div id="container"> <!--Inicio container-->
+
+    <?php
+    include 'cabecalho.html';
+    ?>
+
+    <!--Inicio conteudo-->
+    <div id="conteudo">
+        <!--por questão de acessibilidade do usuário coloca o conteudo antes do menu lateral-->
+
+        <div id="primario"> <!--conteudo primario-->
+            primario
+        </div>
+
+        <div id="secundario"> <!--conteudo secundario-->
+            secundario 
+        </div>
+
+        <div id="lateral"> <!--lateral-->
+            lateral
+        </div>
+
+    </div>
+    <!--fim canteudo-->
+
+
+    </div><!--fim container-->
+
+    <?php
+    include 'rodape.html';
+    ?>
+    
+</body>
+
+</html>
